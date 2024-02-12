e09ebf53541098206a5e3b61f3ab3afa05c4eb66
Beck <becksons@bu.edu>
2024-02-12 01:36:40 -0500

    Fixed code structure

--- app/src/main/java/com/example/beck_baz_hangman/MainActivity.kt
+++ app/src/main/java/com/example/beck_baz_hangman/MainActivity.kt
@@ -7,47 +7,13 @@ import android.widget.ImageView
 
 
 
-class HangmanGame {
 
-
-
-
-    companion object {
-        private var incorrectGuesses = 0
-        fun getImageResource(): Int {
-            return when (incorrectGuesses) {
-                0 -> R.drawable.hangman_0
-                1 -> R.drawable.hangman_1
-                2 -> R.drawable.hangman_2
-                3 -> R.drawable.hangman_3
-                4 -> R.drawable.hangman_4
-                5 -> R.drawable.hangman_5
-                6 -> R.drawable.hangman_6
-                else -> R.drawable.hangman_0
-            }
-        }
-        fun resetGame() {
-            incorrectGuesses = 0
-        }
-        fun onWrongGuess() {
-            incorrectGuesses++
-            if (incorrectGuesses > 6) {
-                resetGame()
-            }
-        }
-
-
-
-        fun isGameOver(): Boolean {
-            return incorrectGuesses > 6
-        }
-    }
-}
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var hangmanImageView: ImageView
     private lateinit var btnIncorrectGuess: Button
+    private var incorrectGuesses = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -63,17 +29,43 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun updateImage() {
-        hangmanImageView.setImageResource(HangmanGame.getImageResource())
+        hangmanImageView.setImageResource(getImageResource())
     }
 
     private fun handleWrongGuess() {
-        HangmanGame.onWrongGuess()
+        onWrongGuess()
         updateImage()
-        if (HangmanGame.isGameOver()) {
+        if (isGameOver()) {
             // TODO: Show game over message
-            HangmanGame.resetGame()
+            resetGame()
             updateImage()
         }
+    }
+
+    private fun getImageResource(): Int {
+        return when (incorrectGuesses) {
+            0 -> R.drawable.hangman_0
+            1 -> R.drawable.hangman_1
+            2 -> R.drawable.hangman_2
+            3 -> R.drawable.hangman_3
+            4 -> R.drawable.hangman_4
+            5 -> R.drawable.hangman_5
+            6 -> R.drawable.hangman_6
+            else -> R.drawable.hangman_0
+        }
+    }
+    private fun resetGame() {
+        incorrectGuesses = 0
+    }
+    private fun onWrongGuess() {
+        incorrectGuesses++
+        if (incorrectGuesses > 6) {
+            resetGame()
+        }
+    }
+
+    private fun isGameOver(): Boolean {
+        return incorrectGuesses > 6
     }
 
 
