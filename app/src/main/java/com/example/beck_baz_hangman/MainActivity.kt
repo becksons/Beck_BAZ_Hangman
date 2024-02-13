095759aadee02bd60e9e73a227bfbf7a92e0aff9
Beck <becksons@bu.edu>
2024-02-13 16:18:43 -0500

    Fragment added

--- app/src/main/java/com/example/beck_baz_hangman/MainActivity.kt
+++ app/src/main/java/com/example/beck_baz_hangman/MainActivity.kt
@@ -2,73 +2,17 @@ package com.example.beck_baz_hangman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var hangmanImageView: ImageView
-    private lateinit var btnIncorrectGuess: Button
-    private var incorrectGuesses = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        hangmanImageView = findViewById(R.id.hangmanImageView)
-        btnIncorrectGuess = findViewById(R.id.btnIncorrectGuess)
-
-        btnIncorrectGuess.setOnClickListener {
-            handleWrongGuess()
-        }
-
-
-    }
-    private fun updateImage() {
-        hangmanImageView.setImageResource(getImageResource())
-    }
-
-    private fun handleWrongGuess() {
-        onWrongGuess()
-        updateImage()
-        if (isGameOver()) {
-            // TODO: Show game over message
-            resetGame()
-            updateImage()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HangmanFragment())
+                .commit()
         }
     }
-
-    private fun getImageResource(): Int {
-        return when (incorrectGuesses) {
-            0 -> R.drawable.hangman_0
-            1 -> R.drawable.hangman_1
-            2 -> R.drawable.hangman_2
-            3 -> R.drawable.hangman_3
-            4 -> R.drawable.hangman_4
-            5 -> R.drawable.hangman_5
-            6 -> R.drawable.hangman_6
-            else -> R.drawable.hangman_0
-        }
-    }
-    private fun resetGame() {
-        incorrectGuesses = 0
-    }
-    private fun onWrongGuess() {
-        incorrectGuesses++
-        if (incorrectGuesses > 6) {
-            resetGame()
-        }
-    }
-
-    private fun isGameOver(): Boolean {
-        return incorrectGuesses > 6
-    }
-
 
 }
-
-
